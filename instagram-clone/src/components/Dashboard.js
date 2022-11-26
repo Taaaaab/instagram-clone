85aa7955327a3d7fe6525d20f1c462b2b3f0d9d7
Taaaaab <owen.whitsett@gmail.com>
2022-11-26 11:50:39 -0500

    working choose file and upload image buttons which save photo to firebase storage and create new image box

--- instagram-clone/src/components/Dashboard.js
+++ instagram-clone/src/components/Dashboard.js
@@ -15,9 +15,14 @@ import {
   orderBy,
   limit,
 } from 'firebase/firestore';
+import {
+  ref, uploadBytes, listAll, getDownloadURL,
+} from 'firebase/storage';
+import { v4 } from 'uuid';
 import instagram from '../images/instagram.png';
 import search from '../images/search.svg';
-import profile from '../images/profile2.png';
+import profileShally from '../images/profile2.png';
+import profileDefault from '../images/profile.png';
 import bushBaby from '../images/bushbaby.jpeg';
 import HeartImage from './HeartImage';
 import CommentImage from './CommentImage';
@@ -25,10 +30,12 @@ import ShareImage from './ShareImage';
 import ShareImage2 from './ShareImage2';
 import '../Dashboard.css';
 import { UserAuth } from '../context/AuthContext';
-import { auth, db } from '../firebase-config';
+import { auth, db, storage } from '../firebase-config';
 
 function Dashboard() {
   const { user, logout } = UserAuth();
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
   const [likes, setLikes] = useState([]);
   const [error, setError] = useState('');
   const navigate = useNavigate();
@@ -124,6 +131,28 @@ function Dashboard() {
     }
   }
 
+  const uploadImage = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  const imageListRef = ref(storage, 'images/');
+
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
   return (
     <div className="container">
       <header className="nav-bar">
@@ -132,15 +161,64 @@ function Dashboard() {
           <img alt="search" className="Search-img" src={search} />
           <input className="Search-input" placeholder="Search" />
         </div>
+        <input type="file" onChange={(event) => { setImageUpload(event.target.files[0]); }} />
+        <button className="Upload-image" onClick={uploadImage}> Upload Image</button>
         <div className="Login-signup">
           @
           {user && user.displayName}
           <button onClick={handleLogout} id="sign-in" className="Login">Log Out</button>
         </div>
       </header>
+      {imageList.map((url) => (
+        <div className="Img-box">
+          <div className="Img-top">
+            <img id="user-pic" className="Profile-pic" alt="Profile-pic" src={profileDefault} />
+            <div className="Name-box">
+              <div className="Name">{user.displayName}</div>
+              <div id="user-name" className="Username">
+                @
+                {user.displayName}
+              </div>
+            </div>
+          </div>
+          <div className="Img-content">
+            <img className="Img-upload" alt="user pic" src={url} />
+          </div>
+          <div className="Img-icons">
+            <div>
+              <HeartImage onClick={heartClick} fill={isActive ? 'red' : 'none'} stroke={isActive ? 'none' : 'currentColor'} />
+              <CommentImage onClick={saveMessage} />
+              <ShareImage onClick={() => window.location = `mailto:${user.email}`} />
+            </div>
+            <div className="share2">
+              <ShareImage2 onClick={() => window.location = `mailto:${user.email}`} />
+            </div>
+          </div>
+          <div className="Img-comments">
+            <div className="likes">
+              {likes.map((like) => <div key={like.id}>{like.data.likes}</div>)}
+    &nbsp;likes
+            </div>
+            <button onClick={() => getComments()} className="view-all">View All Comments</button>
+            <ul className="comments">
+              {comments.map((comment) => (
+                <li key={comment.id}>
+                  <strong>{comment.data.username}</strong>
+    &nbsp;
+                  {comment.data.text}
+                </li>
+              ))}
+            </ul>
+            <span className="line" />
+            <form onSubmit={saveMessage}>
+              <input className="Add-comment" ref={commentRef} type="text" placeholder="Add a comment..." />
+            </form>
+          </div>
+        </div>
+      ))}
       <div className="Img-box">
         <div className="Img-top">
-          <img id="user-pic" className="Profile-pic" alt="Profile-pic" src={profile} />
+          <img id="user-pic" className="Profile-pic" alt="Profile-pic" src={profileShally} />
           <div className="Name-box">
             <div className="Name">Owen</div>
             <div id="user-name" className="Username">@owen</div>
